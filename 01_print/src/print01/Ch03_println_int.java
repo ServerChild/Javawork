838246ac1f9b5d6ee0b60b7b01d899daa9b69a3e
HStack <heosy8706@gmail.com>
2023-10-18 14:06:30 +0900

    println_int2
    주석 수정

--- 01_print/src/print01/Ch03_println_int.java
+++ 01_print/src/print01/Ch03_println_int.java
@@ -5,14 +5,13 @@ public class Ch03_println_int {
 	public static void main(String[] args) {
 		// 문자열일 때 +, - 로 연산하고 싶으면 반드시 괄호를 넣어야 함
 		
-		// 덧셈
-		System.out.println("30 + 20 = " + (30 + 20));
-		// 뺄셈
-		System.out.println("30 - 20 = " + (30 - 20));
-		// 곱셈
-		System.out.println("30 * 20 = " + 30 * 20);
-		// 나눗셈
-		System.out.println("30 / 20 = " + 30 / 3);
+		System.out.println("30 + 20 = " + (30 + 20)); // 덧셈
+		
+		System.out.println("30 - 20 = " + (30 - 20)); // 뺄셈
+		
+		System.out.println("30 * 20 = " + 30 * 20); // 곱셈
+		
+		System.out.println("30 / 20 = " + 30 / 3); // 나눗셈
 	}
 
 }
