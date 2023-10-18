724b80e067aec6c9d39883d4a21c7b82b893aebc
HStack <heosy8706@gmail.com>
2023-10-18 13:56:20 +0900

    02_println의 문자, 숫자
    println의 문자, 숫자

---
+++ 01_print/src/print01/Ch02_println_String.java
@@ -0,0 +1,21 @@
+package print01;
+
+public class Ch02_println_String {
+
+	public static void main(String[] args) {
+		System.out.println("문자열 " + "두번째 문자열");
+		
+		// 문자열이 먼저 나오면 그 뒤는 모두 문자열로 인식
+		System.out.println("문자열 " + 10 + 20);
+		
+		// 숫자로 시작하면 문자열이 나오기 전까지 숫자로 인식
+		System.out.println(10 + 20 + " 문자열");
+		
+		// 앞 숫자는 연산된 결과, 뒷 숫자는 문자열로 출력
+		System.out.println(10 + 20 + "문자열" + 10 + 20);
+		
+		// 뒷 숫자를 연산된 결과로 출력할 시에는 ()로 연산을 묶음
+		System.out.println(10 + 20 + "문자열" + (10 + 20));
+	}
+
+}
---
+++ 01_print/src/print01/Ch03_println_int.java
@@ -0,0 +1,18 @@
+package print01;
+
+public class Ch03_println_int {
+
+	public static void main(String[] args) {
+		// 문자열일 때 +, - 로 연산하고 싶으면 반드시 괄호를 넣어야 함
+		
+		// 덧셈
+		System.out.println("30 + 20 = " + (30 + 20));
+		// 뺄셈
+		System.out.println("30 - 20 = " + (30 - 20));
+		// 곱셈
+		System.out.println("30 * 20 = " + 30 * 20);
+		// 나눗셈
+		System.out.println("30 / 20 = " + 30 / 3);
+	}
+
+}
