25d38aae755c18a452a68e73dfde8f681829b69d
HStack <heosy8706@gmail.com>
2023-10-23 17:57:48 +0900

    Update ex06.java

--- ex/src/ex_20231023/ex06.java
+++ ex/src/ex_20231023/ex06.java
@@ -6,6 +6,7 @@ public class ex06 {
 		// 배열 선언
 		int arr[] = new int[5];
 		
+		// 배열 값 넣음
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = i * 3;
 			if(i == 4)
@@ -13,5 +14,13 @@ public class ex06 {
 			else
 				System.out.print(arr[i] + ", ");
 		}
+		
+		// 합계
+		int sum = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println(sum);
 	}
 }
