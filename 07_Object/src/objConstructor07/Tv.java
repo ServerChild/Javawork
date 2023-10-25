76381efd226d0103f047bbb6480057b6c8117a94
HStack <heosy8706@gmail.com>
2023-10-25 17:48:27 +0900

    20231025_Update5

--- 07_Object/src/objConstructor07/Tv.java
+++ 07_Object/src/objConstructor07/Tv.java
@@ -6,10 +6,44 @@ public class Tv {
 	String inch;
 	int vol;
 	int ch;
+	boolean on;
 	
 	Tv(String company, String model, String inch){
 		this.company = company;
 		this.model = model;
 		this.inch = inch;
 	}
+	
+	// Tv 키거나 끄기
+		void on() {
+			on = !on;
+			if(on)
+				System.out.println("Tv를 켭니다.");
+			else
+				System.out.println("Tv를 끕니다.");
+		}
+		
+		// 채널 올리기(2채널씩 올리기), 매개변수 x
+		int chUp() {
+			ch += 2;
+			return ch;
+		}
+		
+		// 채널 내리기(2채널씩 내리기), 매개변수 x
+		int chDown() {
+			ch -= 2;
+			return ch;
+		}
+		
+		// 채널 바꾸기(랜덤)
+		int chChage() {
+			ch = (int)(Math.random() * 100) + 1;
+			return ch;
+		}
+		
+		// 볼륨 바꾸기, 매개변수 o
+		int Vol(int vol) {
+			this.vol = vol;
+			return this.vol;
+		}
 }
--- 07_Object/src/objConstructor07/TvTest.java
+++ 07_Object/src/objConstructor07/TvTest.java
@@ -3,7 +3,6 @@ package objConstructor07;
 public class TvTest {
 	public static void main(String[] args) {
 		Tv tv1 = new Tv("삼성", "아날로그 TV", "45인치");
-		
 		System.out.println(tv1.company);
 		System.out.println(tv1.model);
 		System.out.println(tv1.inch);
@@ -11,9 +10,33 @@ public class TvTest {
 		System.out.println();
 		
 		Tv tv2 = new Tv("LG", "디지털 TV", "50인치");
-		
 		System.out.println(tv2.company);
 		System.out.println(tv2.model);
 		System.out.println(tv2.inch);
+		
+		System.out.println();
+		
+		Tv tv3 = new Tv("LG", "고급 TV", "100인치");
+		System.out.println(tv3.company);
+		System.out.println(tv3.model);
+		System.out.println(tv3.inch);
+		
+		System.out.println();
+		
+		// tv1 켜기
+		tv1.on();
+				
+		// 채널 이동
+		System.out.println("1번 TV 현재 채널 : " + tv1.chUp());
+		System.out.println("1번 TV 현재 채널 : " + tv1.chUp());
+		System.out.println("1번 TV 현재 채널 : " + tv1.chChage());
+		System.out.println("1번 TV 현재 채널 : " + tv1.chDown());
+		System.out.println();
+				
+		// 볼륨 조절
+		System.out.println("1번 TV 현재 볼륨 : " + tv1.Vol(13));
+				
+		// tv1 끄기
+		tv1.on();
 	}
 }
